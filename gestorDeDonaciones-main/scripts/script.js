6db184b6470a6219bc6913b1b89f9745b96f131e
danielbarberoarroyo <danielbarberoarroyo@gmail.com>
2025-10-05 12:33:54 +0200

    acabado proyecto

--- gestorDeDonaciones-main/scripts/script.js
+++ gestorDeDonaciones-main/scripts/script.js
@@ -1,5 +1,4 @@
-// Creo el array de objetos, con nombre y el importe
-
+// Array de organizaciones
 let organizaciones = [
     { nombre: "Amigos de la tierra", importe: 5 },
     { nombre: "Cáritas", importe: 7 },
@@ -13,10 +12,42 @@ let organizaciones = [
     { nombre: "WWF", importe: 25 }
 ];
 
-// Declaramos las variables
-
+// Variables para donaciones
 let donaciones = {};
 let totalDonado = 0;
 let totalAportaciones = 0;
-let donacionMedia = 0;
-let tramiteFinalizado = false;
+
+// Seleccionar imágenes y añadir eventos
+let articulos = document.querySelectorAll("#container article img");
+for (let i = 0; i < articulos.length; i++) {
+    articulos[i].onclick = function () {
+        let nombre = organizaciones[i].nombre;
+        let importe = organizaciones[i].importe;
+        if (donaciones[nombre]) {
+            donaciones[nombre]++;
+        } else {
+            donaciones[nombre] = 1;
+        }
+        totalDonado += importe;
+        totalAportaciones++;
+    };
+}
+
+// Botón para finalizar trámite
+document.getElementById("finalizar").onclick = function () {
+    let nombres = Object.keys(donaciones).sort().reverse();
+    let resultado = "";
+    for (let i = 0; i < nombres.length; i++) {
+        resultado += nombres[i] + " ---- " + donaciones[nombres[i]] + " aportacion";
+        if (donaciones[nombres[i]] > 1) resultado += "es";
+        resultado += "<br>";
+    }
+    resultado += "<br>Donación final: " + totalDonado + "€<br>";
+    resultado += "Donación media: " + (totalAportaciones ? (totalDonado / totalAportaciones).toFixed(2) : "0") + " €/aportación";
+    document.getElementById("resultado").innerHTML = resultado;
+
+    // Reiniciar variables para nueva serie de donaciones
+    donaciones = {};
+    totalDonado = 0;
+    totalAportaciones = 0;
+};
